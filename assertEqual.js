3d13ff233ba8da8163183e927986a8142113a4c6
jvarghe <vcs.inbox@outlook.com>
2023-02-25 23:27:20 +0000

    Converted string concatention to use template literals. Added some test cases.

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -13,4 +13,7 @@ const assertEqual = function(actual, expected) {
 // Test Code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("lighthouselabs", "lighthouselabs");
+assertEqual("lighthouse labs", "lighthouselabs");
+assertEqual(1234, 1234);
 assertEqual(1243, 1234);
