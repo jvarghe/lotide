fe013ae9831942158e155bf45fd5eb329807edab
jvarghe <vcs.inbox@outlook.com>
2023-02-26 22:24:31 +0000

    Completed a working solution in 'eqArrays.js'.

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,36 @@
 // false.
 const eqArrays = function(array1, array2) {
 
-  let areArraysEqual = null;
+  let elementEquality = true;
+
+  // Check if the two arrays are of equal size, else return false.
+  if(array1.length !== array2.length) { 
+    return false; 
+  } else {
+    // If they are of the same size, compare elements to see if they're the 
+    // same. Return false if this is not true.
+    for (let i = 0; i < array1.length; i++) {
+
+      // If element i of array1 is equal to element i of array2, return true.
+      // Otherwise, mark elementEquality as false.
+      if (array1[i] === array2[i]) {
+        elementEquality = true;
+      } else {
+        elementEquality = false;
+        break;
+      }
+    }
+
+
+    return elementEquality;
+  }
+
 };
 
 
 
 // ASSERTEQUAL() Function Implementation
+// Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -23,11 +47,8 @@ const assertEqual = function(actual, expected) {
 
 
 
-
-
-
 // TEST CASES:
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);             // => Should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);            // => Should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => Should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);           // => Should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  // => Should FAIL
