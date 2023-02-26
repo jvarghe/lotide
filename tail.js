1600ad36b3c9c689125f3e972ebaea24548176c0
jvarghe <vcs.inbox@outlook.com>
2023-02-26 01:37:37 +0000

    Completed the 'tails.js' implementation and tests.

--- tail.js
+++ tail.js
@@ -21,3 +21,14 @@ const assertEqual = function(actual, expected) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // No need to capture the return value since we're not checking it.
 assertEqual(words.length, 3); // The original array should still have 3 elements.
+
+// Test an empty array:
+let empty = [];
+tail(empty);
+assertEqual(empty.length, 0);
+
+// Test an array with only one element:
+let oneElement = ["giant"];
+tail(oneElement);
+// array.length - 1 = 0; Therefore, we expect the return value to be 0.
+assertEqual(oneElement.length - 1, 0);
