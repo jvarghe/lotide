6d824f76d9ac54061d4b0530493230f7d8e9da3a
jvarghe <vcs.inbox@outlook.com>
2023-02-26 00:54:14 +0000

    Added a few more test cases.

--- head.js
+++ head.js
@@ -19,7 +19,11 @@ const assertEqual = function(actual, expected) {
 // assertEqual() Test Cases
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
 assertEqual(head(["tom", "dick", "harry"]), "tom");
 assertEqual(head([28349, 183, 9834.86, 1]), 28349);
 assertEqual(head(["2.34", 284, "cats"]), "2.34");
+
+assertEqual(head(["Supercalifragilisticexpialidocious"]),"Supercalifragilisticexpialidocious"); 
+assertEqual(head([]), undefined);
 
