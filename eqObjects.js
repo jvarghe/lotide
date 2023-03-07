e693ed2d0242280daf1c4203a1322c0965f4baf7
jvarghe <vcs.inbox@outlook.com>
2023-03-07 02:32:07 +0000

    Added a few extra lines of comment to 'eqObjects.js'.

--- eqObjects.js
+++ eqObjects.js
@@ -93,7 +93,10 @@ const eqObjects = function(object1, object2) {
     // Iterate over the keys in `object1`...
     for (const key in object1) {
 
-      // ... check if `object2` has a key listed in `object1`.
+      // ... check if `object2` has a key listed in `object1`. If so, the two
+      // keys must be compared. But a key could be a primitive, an array or 
+      // an object (including functions). We will ignore most objects in this 
+      // project, but we must compare keys if they are primitives or arrays.
       if (Object.hasOwnProperty.call(object2, key)) {
         
         // CHECK 1: IF A KEY IS AN ARRAY
