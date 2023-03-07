b3e94af1d8088aaf383fe90a28f6c383328ea386
jvarghe <vcs.inbox@outlook.com>
2023-03-07 00:03:28 +0000

    Added additional tests to 'findKeyByValue.js'.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -51,5 +51,8 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
