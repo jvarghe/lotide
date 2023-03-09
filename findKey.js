e05c8d156b9613b32c372473160991ce42940c27
jvarghe <vcs.inbox@outlook.com>
2023-03-09 04:32:07 +0000

    Ran ESLint and fixed all style errors in 'findKey.js'.

--- findKey.js
+++ findKey.js
@@ -1,23 +1,23 @@
 /* CHALLENGE
  *
- * Not too long ago, we implemented a `findKeyByValue()` function in Lotide. It 
- * was similar to, but more limited than, `findKey()` from Lodash. Now that we 
- * know about callbacks, let's make a more complex version of this in a similar 
+ * Not too long ago, we implemented a `findKeyByValue()` function in Lotide. It
+ * was similar to, but more limited than, `findKey()` from Lodash. Now that we
+ * know about callbacks, let's make a more complex version of this in a similar
  * way to what Lodash has.
- * 
- * Here is Lodash's documentation for its `findKey()` function: 
+ *
+ * Here is Lodash's documentation for its `findKey()` function:
  * https://lodash.com/docs/4.17.11#findKey
- * 
- * NOTE: Lodash's example code for `findKey()` shows us that instead of just 
- * supporting callback functions, it can support "iteratee shorthand" using 
- * objects and arrays. We can safely ignore these examples since our function 
+ *
+ * NOTE: Lodash's example code for `findKey()` shows us that instead of just
+ * supporting callback functions, it can support "iteratee shorthand" using
+ * objects and arrays. We can safely ignore these examples since our function
  * will be simpler and not need to support this.
- * 
- * Implement the function `findKey()` which takes in an object and a callback. 
- * It should scan the object and return the first key for which the callback 
- * returns a truthy value. If no key is found, then it should return 
+ *
+ * Implement the function `findKey()` which takes in an object and a callback.
+ * It should scan the object and return the first key for which the callback
+ * returns a truthy value. If no key is found, then it should return
  * `undefined`.
- * 
+ *
  * Tip: Consult `findKeyByValue.js`.
  */
 
@@ -33,8 +33,8 @@ const assertEqual = function(actual, expected) {
 
 
 // FINDKEY()
-// This function takes an object and a callback function. It should scan the 
-// object and return the first key for which the callback returns a truthy 
+// This function takes an object and a callback function. It should scan the
+// object and return the first key for which the callback returns a truthy
 // value. If no key is found, then it should return `undefined`.
 const findKey = function(objectToScan, callBack) {
   
@@ -61,7 +61,7 @@ const findKey = function(objectToScan, callBack) {
 
 
 // TEST CASES
-// Test Case 1: 
+// Test Case 1:
 const object1 = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
