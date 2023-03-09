0db404166dd7f3b79ccbfcf1bd7c4143f3a8a85a
jvarghe <vcs.inbox@outlook.com>
2023-03-09 03:15:29 +0000

    Ran ESLint and fixed all stylistic issues.

--- takeUntil.js
+++ takeUntil.js
@@ -1,25 +1,25 @@
 /* CHALLENGE
  *
- * Lodash has a `takeWhile()` function, the documentation for which can be found 
+ * Lodash has a `takeWhile()` function, the documentation for which can be found
  * here: https://lodash.com/docs/4.17.11#takeWhile
- * 
- * Lodash's example code for `takeWhile()` shows us that instead of just 
- * supporting callback functions, it can support "iteratee shorthand" using 
- * objects and arrays. We can safely ignore these examples since our function 
+ *
+ * Lodash's example code for `takeWhile()` shows us that instead of just
+ * supporting callback functions, it can support "iteratee shorthand" using
+ * objects and arrays. We can safely ignore these examples since our function
  * will be simpler and not need to support this.
- * 
- * The challenge is to implement a function quite similar to this one, but isn't 
- * present in their library: `takeUntil()`. It will take in two parameters as 
- * well: 
- * 
+ *
+ * The challenge is to implement a function quite similar to this one, but isn't
+ * present in their library: `takeUntil()`. It will take in two parameters as
+ * well:
+ *
  *   1. The array to work with
  *   2. The callback (which Lodash calls "predicate")
- * 
- * `takeUntil()` will keep collecting items from a provided array until the 
+ *
+ * `takeUntil()` will keep collecting items from a provided array until the
  * callback provided returns a truthy value. In short, the function is to return
  * a slice of the array until the callback stops the process.
- * 
- * To keep things simple, the callback should only be provided one value: The 
+ *
+ * To keep things simple, the callback should only be provided one value: The
  * item in the array.
  */
 
@@ -72,24 +72,24 @@ const eqArrays = function(array1, array2) {
 
 
 
-// TAKEUNTIL() 
-// This function takes an array and a callback function. It will iterate over 
+// TAKEUNTIL()
+// This function takes an array and a callback function. It will iterate over
 // the array, applying the callback until the predicate (aka callback function)
-// returns a truthy value. Then it will return an array containing the values 
-// all the values until the predicate terminates the process. 
+// returns a truthy value. Then it will return an array containing the values
+// all the values until the predicate terminates the process.
 const takeUntil = function(array, callback) {
   
   // Create an array to hold results.
   let resultArray = [];
 
-  // Iterate over the array... 
+  // Iterate over the array...
   for (const element of array) {
 
     // ... call the callback function and pass in elements of the array. Keep
-    // doing that until the predicate returns a truthy value. 
-    if(callback(element) !== true) {
+    // doing that until the predicate returns a truthy value.
+    if (callback(element) !== true) {
 
-      // If the predicate function returns a falsy value, push the element 
+      // If the predicate function returns a falsy value, push the element
       // on to the resultArray.
       resultArray.push(element);
 
@@ -107,18 +107,18 @@ const takeUntil = function(array, callback) {
 
 
 
-// TEST CASES: 
+// TEST CASES:
 
-// Test 1: 
+// Test 1:
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 
-console.log('---');
+console.log('----------------------------------------------------');
 
-// Test 2: 
+// Test 2:
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
