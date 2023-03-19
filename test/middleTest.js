5e4c57ebfa7f862cfbe3c57d0c7a059a0a89c28b
jvarghe <vcs.inbox@outlook.com>
2023-03-19 01:07:26 +0000

    Converted manual tests in 'middleTest.js' to use Mocha and Chai for tests.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,26 +1,58 @@
-// IMPORTS FOR TESTS
+// MANUAL TESTS
+// 
+// // IMPORTS FOR TESTS
+// const middle = require("../middle.js");
+// const assertArraysEqual = require("../assertArraysEqual.js");
+
+
+// // TEST CASES
+
+// // Testing an empty array:
+// let result1 = middle([]);                                       // => [];
+// assertArraysEqual(result1, []);
+
+// // Testing an array with a single element:
+// let result2 = middle(["Supercalifragilisticexpialidocious"]);   // => [];
+// assertArraysEqual(result2, []);
+
+// // Testing an array with two elements:
+// let result3 = middle(["Higgs", "Boson"]);                       // => [];
+// assertArraysEqual(result3, []);
+
+// // Testing an array with four elements:
+// let result4 = middle([1, 2, 3, 4]);                             // => [2, 3]
+// assertArraysEqual(result4, [2, 3]);
+
+// // Testing an array with mixed elements:
+// let result5 = middle(["Fire", "tang", 3, "zulu", 23.13]);       // => 3
+// assertArraysEqual(result5, [3]);
+
+
+// MOCHA & CHAI TESTS
 const middle = require("../middle.js");
-const assertArraysEqual = require("../assertArraysEqual.js");
+const assert = require("chai").assert;
 
 
-// TEST CASES
+describe("#middle", () => {
 
-// Testing an empty array:
-let result1 = middle([]);                                       // => [];
-assertArraysEqual(result1, []);
+  // Tests an an empty array:
+  it("This test should return `[]`", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-// Testing an array with a single element:
-let result2 = middle(["Supercalifragilisticexpialidocious"]);   // => [];
-assertArraysEqual(result2, []);
+  // Tests an array with a single element:
+  it("This test should return `[]`", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  
+  // Tests an array with four elements:
+  it("This test should return [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-// Testing an array with two elements:
-let result3 = middle(["Higgs", "Boson"]);                       // => [];
-assertArraysEqual(result3, []);
-
-// Testing an array with four elements:
-let result4 = middle([1, 2, 3, 4]);                             // => [2, 3]
-assertArraysEqual(result4, [2, 3]);
-
-// Testing an array with mixed elements:
-let result5 = middle(["Fire", "tang", 3, "zulu", 23.13]);       // => 3
-assertArraysEqual(result5, [3]);
+  // Tests an array with mixed elements:
+  it("This test should return `[3]`", () => {
+    assert.deepEqual(middle(["Fire", "tang", 3, "zulu", 23.13]), [3]);
+  });
+  
+});
