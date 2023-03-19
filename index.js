4b81b865b9feaca8b6dcf8183cae0931ce3f6883
jvarghe <vcs.inbox@outlook.com>
2023-03-19 01:42:10 +0000

    Created a single 'index.js' file where all the utility functions were exported as a single object.

---
+++ index.js
@@ -0,0 +1,39 @@
+// PROJECT FUNCTIONS EXPORT FILE
+// 
+// We can exports numbers, strings, functions, and objects using the exports 
+// feature, but in the previous reading, we realized that an object should be 
+// exported with all our Lotide utility functions. 
+// 
+// To do this, we should import all the utility functions into a single file, 
+// namely this one, `index.js`. Then, we should export all these functions as an
+// object. Consolidating all the utility function imports into a single file 
+// allows us skip multi-line imports in each file. We can just import the 
+// `index.js` file into every file that needs it with just a single line of 
+// code: 
+// `const _ = require("./index.js")`. 
+// 
+// So long as we consolidate all the imports and exports into a single file, 
+// this saves us from having to do manually do lengthy imports in every file 
+// that needs these utility functions. 
+
+
+// IMPORTS
+const assertArraysEqual = require("./assertArraysEqual.js");
+const assertEqual = require("./assertEqual.js");
+const eqArrays = require("./eqArrays.js");
+
+const head   = require("./head.js");
+const tail   = require("./tail.js");
+const middle = require("./middle.js");
+
+
+
+// EXPORTS
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
