9946a1e450972a4860d45a9a8bf1323cdb939249
jvarghe <vcs.inbox@outlook.com>
2023-03-21 00:42:23 +0000

    Refactored 'eqObjects.js' to remove functions assertEqual and eqArrays. Also added an export for eqObjects.

--- eqObjects.js
+++ eqObjects.js
@@ -20,50 +20,9 @@
  */
 
 
-
-// EQARRAYS() FUNCTION
-// This function takes in two arrays and compares their elements. Based on
-// whether the contents are identical, the function returns either true or
-// false.
-const eqArrays = function(array1, array2) {
-
-  let elementEquality = true;
-
-  // Check if the two arrays are of equal size, else return false.
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    // If they are of the same size, compare elements to see if they're the
-    // same. Return false if this is not true.
-    for (let i = 0; i < array1.length; i++) {
-
-      // If element i of array1 is equal to element i of array2, return true.
-      // Otherwise, mark elementEquality as false.
-      if (array1[i] === array2[i]) {
-        elementEquality = true;
-      } else {
-        elementEquality = false;
-        break;
-      }
-    }
-
-
-    return elementEquality;
-  }
-
-};
-
-
-
-// ASSERTEQUAL() Function Implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// IMPORTS
+const assertEqual = require("./assertEqual.js");
+const eqArrays = require("./eqArrays.js");
 
 
 // EQOBJECTS() FUNCTION
@@ -142,6 +101,9 @@ const eqObjects = function(object1, object2) {
   return areObjectsEqual;
 };
 
+
+// EXPORTS
+module.exports = eqObjects;
 
 
 // TEST CASES
