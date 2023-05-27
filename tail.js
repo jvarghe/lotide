759da9317ef222deec804e2cce52fd2d231a9d62
jvarghe <vcs.inbox@outlook.com>
2023-05-27 23:41:06 +0000

    Added comments to 'tail.js'.

--- tail.js
+++ tail.js
@@ -1,3 +1,11 @@
+/* CHALLENGE
+ *
+ * Instead of the "head", sometimes we need the "tail" of an array. The tail is
+ * not the last element of the array. The tail is meant to be every element
+ * except the head (first element) of the array.
+ */
+
+
 // TAIL IMPLEMENTATION
 // This function takes an array, extracts from it the "tail", i.e. all elements
 // except the head or first one. It returns this shrunken array.
