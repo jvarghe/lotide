ad222de78a6b2db43866daa12e3bcd33b08c909d
jvarghe <vcs.inbox@outlook.com>
2023-06-05 18:23:56 +0000

    Refactored 'letterPositions.js' to incorporate a lower-cased input string.

--- letterPositions.js
+++ letterPositions.js
@@ -1,6 +1,8 @@
-/* CHALLENGE: Implement a function called `letterPositions()`. This challenge
- * is similar to the `countLetters.js` activity and allows us to spend some
- * more time solving problems with objects.
+/* CHALLENGE
+ *
+ * Implement a function called `letterPositions()`. This challenge is similar
+ * to the `countLetters.js` activity and allows us to spend some more time 
+ * solving problems with objects.
  *
  * We'll implement a new function `letterPositions()` which will return all the
  * indices (zero-based positions) in the string where each character is found.
@@ -8,6 +10,22 @@
  * For each letter, instead of returning just one number to represent its number
  * of occurrences, multiple numbers may be needed to represent all the places
  * in the string that it shows up.
+ * 
+ * PSEUDOCODE
+ * 
+ *  * Create an object to track the indices of each letter. Since we need to
+ *    track the positions of all the characters in the string, the object will
+ *    have keys for each character (`a`, `b` and so on), and each key will 
+ *    contain an array listing the indices of the all the places it occurs.
+ * 
+ *  * Convert sentence to lowercase to avoid case issues.
+ * 
+ *  * Iterate over the string:
+ *      * Remove or ignore unwanted characters (spaces only).
+ *      * Check if existing character has already been added to object.
+ *        * If so, increment it's value by 1.
+ *        * If not, add character and give it a value of 1. 
+ * 
  */
 
 
@@ -17,7 +35,7 @@ const assertArraysEqual = require("./assertArraysEqual.js");
 
 
 
-// LETTERPOSITIONS IMPLEMENTATION
+// LETTERPOSITIONS() IMPLEMENTATION
 //
 // This function takes a `sentence` string. It reads this string and it will
 // make a list of all the indices in the string where each character is found.
@@ -29,32 +47,35 @@ const letterPositions = function(sentence) {
   // Create an object to hold the final results.
   const resultObject = {};
 
+  // Convert the string to lower case to avoid dealing with upper and lower
+  // cases.
+  const LCSentence = sentence.toLowerCase();
+
   
-  // Iterate over the `sentence` string...
-  for (let char = 0; char < sentence.length; char++) {
+  // Iterate over the `LCSentence` string (Use `for` loop to track indices).
+  for (let char = 0; char < LCSentence.length; char++) {
   
-    // If the current character is a space, comma or fullstop, skip over it
-    // and move on to the next element.
-    if ((sentence[char] === " ") || (sentence[char] === ",") ||
-        (sentence[char] === ".")) {
+    // If the current character is a space skip over it and move on to the next
+    // element.
+    if ((LCSentence[char] === " ")) {
       continue;
 
     // If it is an alphanumerical character, prepare to add it to
-    // `sentenceCountObject`.
+    // `resultObject`.
     } else {
 
       // Check if the character has already been added to `resultObject`.
-      if (Object.prototype.hasOwnProperty.call(resultObject, sentence[char]) === true) {
+      if (Object.prototype.hasOwnProperty.call(resultObject, LCSentence[char]) === true) {
 
         // If so, append the position of the current element to the correct
         // key array.
-        resultObject[sentence[char]].push(char);
+        resultObject[LCSentence[char]].push(char);
 
       // If the character does not exist in the `resultObject` object, create
       // the key and add the current index to it.
       } else {
-        resultObject[sentence[char]] = [];
-        resultObject[sentence[char]].push(char);
+        resultObject[LCSentence[char]] = [];
+        resultObject[LCSentence[char]].push(char);
       }
 
     }
@@ -67,13 +88,12 @@ const letterPositions = function(sentence) {
 
 
 
-// TEST CASES:
+// TEST CASES
 // Test cases for the word "hello":
 assertArraysEqual(letterPositions("hello").h, [0]);
 assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello").l, [2, 3]);
 assertArraysEqual(letterPositions("hello").o, [4]);
-
 
 
 // EXPORTS
