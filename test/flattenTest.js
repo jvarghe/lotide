9b9d7debb714cb7425f99c35618fe69f82b6d0db
jvarghe <vcs.inbox@outlook.com>
2023-06-11 01:43:08 +0000

    Added 2 tests to 'test/flattenTest.js'.

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -16,5 +16,24 @@
 
 
 // MOCHA & CHAI TESTS
+const assert = require("chai").assert;
+const flatten = require("../flatten.js");
 
 
+// This file tests `flatten.js` to check for bugs.
+describe("#flatten", () => {
+
+  it("The Flattened Array Should be: [1, 2, 3, 4, 5, 6]", () => {
+    const unflattenedArray = [1, 2, [3, 4], 5, [6]];
+    const flattenedArray = [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(flatten(unflattenedArray), flattenedArray);
+  });
+
+
+  it("The Flattened Array Should be: [\"Jack\", \"and\", \"Jill\", \"went\", \"up\", \"the\", \"hill\"]", () => {
+    const unflattenedArray = [[ "Jack", "and", "Jill"], "went", "up", ["the"], "hill"];
+    const flattenedArray = ["Jack", "and", "Jill", "went", "up", "the", "hill"];
+    assert.deepEqual(flatten(unflattenedArray), flattenedArray);
+  });
+
+});
