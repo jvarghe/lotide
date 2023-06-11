21bc89fac78884dde72b282dffb33ca58527d4c1
jvarghe <vcs.inbox@outlook.com>
2023-06-11 00:38:38 +0000

    Moved test files from 'countOnly.js' to 'countOnlyTest.js'.

---
+++ test/countOnlyTest.js
@@ -0,0 +1,28 @@
+// MANUAL TESTS
+
+// IMPORTS FOR MANUAL TESTS
+// const assertEqual = require("./assertEqual.js");
+
+// An array containing first names.
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
+
+
+// // A the result of a call to countOnly(), passing in an object.
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+
+// // TEST CASES:
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
