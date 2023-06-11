3141f37be7c6898d15ed09369594d671b275432c
jvarghe <vcs.inbox@outlook.com>
2023-06-11 00:58:23 +0000

    Added 4 tests to 'test/countOnlyTest.js'.

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -26,3 +26,48 @@
 // assertEqual(result1["Karima"], undefined);
 // assertEqual(result1["Fang"], 2);
 // assertEqual(result1["Agouhanna"], undefined);
+
+
+
+// MOCHA & CHAI TESTS
+const assert = require("chai").assert;
+const countOnly = require("../countOnly.js");
+
+
+// This file tests `countOnly.js` to check for bugs.
+// Primitive Type Tests
+describe("#countOnly", () => {
+
+  // This array will be used by all the tests. [Duplicate key `Salima` is used for testing.]
+  const namesArray = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+
+
+  it("Equality check should return an object: { Fang: 2, Jason: 1 }.", () => {
+    const truthyNamesObject = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    const expectedOutput = { Fang: 2, Jason: 1 };
+    assert.deepEqual(countOnly(namesArray, truthyNamesObject), expectedOutput);
+  });
+
+
+  it("Equality check should return an object: { Kavith: 1, Jason: 1 }.", () => {
+    const truthyNamesObject = { "Jason": true, "Karima": true, "Fang": false, "Kavith": true };
+    const expectedOutput = { Kavith: 1, Jason: 1 };
+    assert.deepEqual(countOnly(namesArray, truthyNamesObject), expectedOutput);
+  });
+
+
+  it("Equality check should return an object: { Karl: 1, Salima: 2, Joe: 1 }.", () => {
+    const truthyNamesObject = { "Salima": true, "Karl": true, "Salima": true, "Joe": true };
+    const expectedOutput = { Karl: 1, Salima: 2, Joe: 1 };
+    assert.deepEqual(countOnly(namesArray, truthyNamesObject), expectedOutput);
+  });
+
+
+  it("Equality check should return an object: { Karl: 1, Agouhanna: 1, Joe: 1 }.", () => {
+    const truthyNamesObject = { "Agouhanna": true, "Karl": true, "Salima": false, "Joe": true };
+    const expectedOutput = { Karl: 1, Agouhanna: 1, Joe: 1 };
+    assert.deepEqual(countOnly(namesArray, truthyNamesObject), expectedOutput);
+  });
+
+});
+
